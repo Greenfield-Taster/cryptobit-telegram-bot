541ae9c9a5173c6e5719d4f8cbe3ecbff7f13d94
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-02-28 14:41:19 +0200

    Add delete and put methods

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 app.use(express.json());
